ccb1c54ffe5cd84f88c0f1c81f4b417ae07c2d29
Kevin Lacaille <kevinlacaille@gmail.com>
2019-04-25 11:24:59 -0400

    New IDs w/ correct redshifts

--- h1700_spectra.py
+++ h1700_spectra.py
@@ -13,15 +13,16 @@ ld		new		z
 ---		---		-
 2b		4.1		2.318
 4a		7.1		2.313
-16a		17.1	2.306
 5.X		5.2		2.303
 13.Y	16.1	1.575
+16a		17.1	2.306
 '''
 
+
 # Temp. ID names
-IDs = np.array(['2bH-1d','2bK_1d','4aH_1d','4aJ2_1d','4aK_1d','16aK_1d'])
+IDs = np.array(['4','5_2','7_1','16','17'])
 # Temp. spec. redshifts
-z = np.array([None,2.318,None,None,2.31,2.3105])
+z = np.array([2.318,2.303,2.313,1.575,2.306])
 
 # Import Gemini spectra
 wavelength = []
@@ -40,31 +41,31 @@ lines_names = ['[N I]','[N II]', r'H$\alpha$','[N II]','[S II]','[S II]']
 # Plot spectra
 ##############
 for i in range(len(IDs)):
-	if IDs[i] == '2bH-1d' or IDs[i] == '4aH_1d' or IDs[i] == '4aJ2_1d':
-		pass
-	else:
-		ax1 = pl.subplot(111)
-		fig = pl.figure(figsize=(8,5))
-		pl.rc('font',size=18)
-		pl.rc('mathtext', default='regular')
+	# if IDs[i] == '2bH-1d' or IDs[i] == '4aH_1d' or IDs[i] == '4aJ2_1d':
+	# 	pass
+	# else:
+	ax1 = pl.subplot(111)
+	fig = pl.figure(figsize=(8,5))
+	pl.rc('font',size=18)
+	pl.rc('mathtext', default='regular')
 
-		pl.plot(wavelength[i]/(1+z[i]), flux[i], c='b', label='1700.'+IDs[i][0:3]+'(z='+str(z[i])+')')
+	pl.plot(wavelength[i]/(1+z[i]), flux[i], c='b', label='1700.'+IDs[i][0:3]+'(z='+str(z[i])+')')
 
-		for j in range(len(lines_wavelength)):
-			pl.axvline(x=lines_wavelength[j],c='k',ls='--')
-			# pl.text(lines_wavelength[j]+5,1,lines_names[j])
+	for j in range(len(lines_wavelength)):
+		pl.axvline(x=lines_wavelength[j],c='k',ls='--')
+		# pl.text(lines_wavelength[j]+5,1,lines_names[j])
 
 
-		pl.ylabel('Relative flux')
-		pl.xlabel(r'Rest wavelength ($\AA$)')
+	pl.ylabel('Relative flux')
+	pl.xlabel(r'Rest wavelength ($\AA$)')
 
-		if abs(z[i]-2.3)<0.1:
-			pl.xlim(6400,6900)
-		# elif abs(z[i]-1.5)<0.1:
-		#     pl.xlim(4700,5100)
+	# if abs(z[i]-2.3)<0.1:
+	# 	pl.xlim(6400,6900)
+	# elif abs(z[i]-1.5)<0.1:
+	#     pl.xlim(4700,5100)
 
-		pl.ylim(-0.4,1.2)
-		pl.legend(fontsize=14,loc=0,ncol=1,frameon=True,numpoints=1,scatterpoints=1)
+	pl.ylim(-0.4,1.2)
+	pl.legend(fontsize=14,loc=0,ncol=1,frameon=True,numpoints=1,scatterpoints=1)
 
-		pl.savefig('../Figures/HS1700_spectra/1700_'+IDs[i][0:3]+'.pdf', bbox_inches='tight')
-		pl.close()
+	pl.savefig('../Figures/HS1700_spectra/1700_'+IDs[i][0:3]+'.pdf', bbox_inches='tight')
+	pl.close()
